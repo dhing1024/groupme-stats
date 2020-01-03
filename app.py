d2358f12278f16d4d05d0f87657eeea6a71196ba
Dominick <dhing@stanford.edu>
2020-01-03 16:02:12 -0500

    Add support for routines

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import routines
 
 from groups import get_all_groups
 from messageGroup import MessageGroup
@@ -23,11 +24,27 @@ def main():
 			os.makedirs(message_output)
 
 		mg = MessageGroup.from_groupme_id(TOKEN, id)
-		mg.to_html(message_output + "/messages.json")
+		routines.routine_one(mg, message_output)
+		#mg.to_html(message_output + "/All Messages.html")
+		#mg.to_json(message_output + "/messages.json")
+		#mg.timesort(ascending = False).to_html(message_output + "/All Messages (Reverse Order).html")
+		#mg.timesort(ascending = False).to_json(message_output + "/messages_reversed.json")
+		#json.dump(mg.info(), open(message_output + "/info.json", 'w'))
+		#user_data_output = message_output + "/user_data"
+		#if not os.path.exists(user_data_output):
+		#	os.makedirs(user_data_output)
 
-		user_data_output = message_output + "/user_data"
-		if not os.path.exists(user_data_output):
-			os.makedirs(user_data_output)
+		#senders = mg.senders()
+		#for item in senders:
+		#	mg.filter_senders(item).to_html(user_data_output + "/" + mg.get_name_from_id(item).replace("/", "-") + ".html")
+
+def execute_routines(mg, configs):
+	configs = json.load(open('config.json', 'r'))
+	TOKEN = configs['token']
+	PATH = configs['path']
+
+
+	return
 
 if __name__ == '__main__':
 	main()
---
+++ routines.py
@@ -0,0 +1,19 @@
+from messageGroup import MessageGroup
+import json
+import os
+
+def routine_one(mg, output):
+	mg.to_html(output + "/All Messages.html")
+	mg.to_json(output + "/messages.json")
+	mg.timesort(ascending = False).to_html(output + "/All Messages (Reverse Order).html")
+	mg.timesort(ascending = False).to_json(output + "/messages_reversed.json")
+	json.dump(mg.info(), open(output + "/info.json", 'w'))
+	user_data_output = output + "/user_data"
+	if not os.path.exists(user_data_output):
+		os.makedirs(user_data_output)
+
+	senders = mg.senders()
+	for item in senders:
+		mg.filter_senders(item).to_html(user_data_output + "/" + mg.get_name_from_id(item).replace("/", "-") + ".html")
+
+	return
